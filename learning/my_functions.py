24c035df5e060d83567f4fd9e6e8d0d487e40f51
Jakub Wójcik <jakub.wojcik@cgm.com>
2024-12-02 12:10:02 +0100

    declared a function and used it

---
+++ learning/my_functions.py
@@ -0,0 +1,5 @@
+def least_difference(a, b, c):
+    diff1 = abs(a - b)
+    diff2 = abs(b - c)
+    diff3 = abs(a - c)
+    return min(diff1, diff2, diff3)
---
+++ learning/using_my_functions.py
@@ -0,0 +1,4 @@
+import my_functions
+from my_functions import least_difference
+
+print(least_difference(55, 4, 21))
