b069084add6b3173ed4f3c989f569ed3751930f9
Jakub Wójcik <jakub.wojcik@cgm.com>
2024-12-04 17:24:42 +0100

    read db connection data from properties file

--- basic_machine_learning_course/database_connection.py
+++ basic_machine_learning_course/database_connection.py
@@ -1,15 +1,20 @@
 import oracledb
 import pandas as pd
 from sqlalchemy import create_engine
+import configparser
+
+#get database properties
+config = configparser.RawConfigParser()
+config.read('config/sql_datasource.properties')
 
 # connect to oracle database
 connection=oracledb.connect(
-     user="user",
-     password="password",
-     dsn="host:port/schema")
+     user=config.get("oracle", "database_user"),
+     password=config.get("oracle", "database_password"),
+     dsn=config.get("oracle", "database_dsn"))
 # create engine
 engine = create_engine('oracle+oracledb://', creator=lambda: connection)
 # Get the data into a DataFrame
-query = "SELECT * FROM X"
+query = "SELECT * FROM Person"
 df = pd.read_sql(query, engine)
 print(df.describe())
