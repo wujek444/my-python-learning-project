2d8485905697225ad03f385e8c4bbb185c00b518
Jakub Wójcik <jakub.wojcik@cgm.com>
2024-12-04 18:05:17 +0100

    delete file data_visualization.html to save space in repo, create 'generated' catalogue with empty placeholder file

--- basic_machine_learning_course/basic_data_exploration.py
+++ basic_machine_learning_course/basic_data_exploration.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 # save filepath to variable for easier access
 melbourne_file_path = 'datasets/melb_data.csv'
+
 # read the data and store data in DataFrame titled melbourne_data
 melbourne_data = pd.read_csv(melbourne_file_path)
-# print a summary of the data in Melbourne data
-# print(melbourne_data.describe())
-print(melbourne_data.to_html(), file=open("resources/data_visualization.html", "w"))
+
+# print a summary of the data in Melbourne data to a html file
+print(melbourne_data.to_html(), file=open("generated/data_visualization.html", "w"))
