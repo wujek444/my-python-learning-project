efeb17a75bc03b680e0e394ce59960d989cab74b
Jakub Wójcik <jakub.wojcik@cgm.com>
2024-12-04 17:24:42 +0100

    read db connection data from properties file

--- basic_machine_learning_course/database_connection.py
+++ basic_machine_learning_course/database_connection.py
@@ -12,8 +12,10 @@ connection=oracledb.connect(
      user=config.get("oracle", "database_user"),
      password=config.get("oracle", "database_password"),
      dsn=config.get("oracle", "database_dsn"))
+
 # create engine
 engine = create_engine('oracle+oracledb://', creator=lambda: connection)
+
 # Get the data into a DataFrame
 query = "SELECT * FROM Person"
 df = pd.read_sql(query, engine)
